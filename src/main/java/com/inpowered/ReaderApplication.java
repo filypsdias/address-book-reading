c78b35a4fb9677c1909aa96a650eeafd6ee56a15
filypsdias <filipedias1708@gmail.com>
2022-11-05 08:14:34 -0300

    :sparkles: Creating project initial structure

---
+++ src/main/java/com/inpowered/ReaderApplication.java
@@ -0,0 +1,17 @@
+package main.java.com.inpowered;
+
+/**
+ * Class responsible for calling file reader and return properly intel.
+ * This class will call a file reader; based on names, genders and birthdays
+ * and return how many males are in the file, who's the oldest person in the file
+ * and how many days older Bill is than Paul.
+ *
+ * @author Filipe Lima
+ * @version 1.0.0
+ */
+public class ReaderApplication {
+
+  //TODO: change that into different classes. Let's do the logic in one place first
+  public static void main(String[] args) {
+  }
+}
